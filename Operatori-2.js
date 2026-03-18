6450bb6ee14f7270c5c7caeb27268d9190eb2ef0
EmanueleSca5 <emanuele.scagliarini05@gmail.com>
2026-03-18 16:43:51 +0100

    Operatori-es-2

---
+++ Operatori-2.js
@@ -0,0 +1,13 @@
+let anno_ema = 2005;
+let anno_corrente = 2026;
+
+let età_ema = anno_corrente - anno_ema;
+
+console.log(età_ema);
+
+let anni100 = (100 - età_ema)
+
+console.log(anni100);
+
+console.log("hai " + età_ema + " e per arrivare a 100 anni ti mancano " + anni100 + " anni");
+
