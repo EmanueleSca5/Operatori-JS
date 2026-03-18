3c79b7b4e113be0dba8f316eee13a495e59ed114
EmanueleSca5 <emanuele.scagliarini05@gmail.com>
2026-03-18 17:11:26 +0100

    Operatori-3

---
+++ Operatori-3.js
@@ -0,0 +1,23 @@
+let a = Number(prompt("Scrivi un numero"))
+let b = Number(prompt("Scrivi un numero"))
+let c = Number(prompt("Scrivi un numero"))
+let d = Number(prompt("Scrivi un numero"))
+
+let somma = a + b + c + d;
+let differenza = a - b - c - d;
+let moltiplicazione = a * b * c * d;
+let divisione = a / b / c / d;
+let potenza = a ** b ** c ** d;
+let modulo = a % b % c % d;
+
+console.log("la somma dei tuoi numeri è " + somma);
+console.log("la differenza dei tuoi numeri è " + differenza);
+console.log("la moltiplicazione tra i tuoi numeri da come risultato " + moltiplicazione);
+console.log("la divisione tra i tuoi numeri da come risultato " + divisione);
+console.log("i tuoi numeri elevati alla potenza tra loro uno dopo l altro da come risultato " + potenza);
+console.log("il resto della divisione tra i tuoi numeri è " + modulo);
+
+
+
+
+
