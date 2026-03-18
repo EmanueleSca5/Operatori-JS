57f4de816838f9071b9b1dd08f64f386c93e4d3a
EmanueleSca5 <emanuele.scagliarini05@gmail.com>
2026-03-18 16:35:04 +0100

    Operatori-es-1

---
+++ Operatori-1.js
@@ -0,0 +1,16 @@
+let a = 3;
+let b = 5;
+let c = 6;
+let d = 2;
+let e = 7;
+
+let somma = a + b + c + d + e;
+let media = somma / 5;
+
+console.log(somma);
+console.log(media);
+
+console.log("la somma delle 5 variabili è " + somma + " e la media delle mie variabili è " + media);
+
+
+
